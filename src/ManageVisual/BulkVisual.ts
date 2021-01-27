bd2ca12be8245d5612959ab8ca4fd5d160d29ce7
linch1 <m.carassiti@hotmail.it>
2021-01-27 12:46:21 +0100

    added extension support to BulkVisual

--- src/ManageVisual/BulkVisual.ts
+++ src/ManageVisual/BulkVisual.ts
@@ -5,14 +5,18 @@ import { Visual } from "./Visual";
 
 export class BulkVisual {
     
-    constructor(public VISUALS_FOLDER: string){}
+    /**
+     * @param VISUALS_FOLDER the folder where the visual are contained
+     * @param extension the extension of the visual files ( php, ejs, html etc...) _without the dot_
+     */
+    constructor(public VISUALS_FOLDER: string, public extension?: string){}
 
     public getAllVisuals(): Visual[]{
         let visuals: Visual[] = []; 
         let visualsFolders = FileReader.getDirectories(this.VISUALS_FOLDER);
         for ( let visualFolder of visualsFolders){
             let visualFolderPath = StringComposeWriter.concatenatePaths(this.VISUALS_FOLDER, visualFolder);
-            visuals.push(new Visual(visualFolderPath));
+            visuals.push(new Visual(visualFolderPath, this.extension));
         }
         return visuals;
     }
