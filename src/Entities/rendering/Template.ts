0aea9ba36f9334b07ef0e6e4052d6c94a0e57f8b
Linch1 <m.carassiti@hotmail.it>
2023-01-02 17:48:44 +0100

    minor edits

---
+++ src/Entities/rendering/Template.ts
@@ -0,0 +1,14 @@
+import { Project } from "../../ManageProjects";
+import { ThemeAux } from "../../ManageTheme/ThemeAux";
+import { View } from "../../ManageView";
+
+export class Template extends View {
+
+  constructor( nullOne: ThemeAux, nullTwo: string ){
+    super('null', new Project(''));
+  }
+  getIncludeFunction(path: string): string {
+    throw new Error("Method not implemented.");
+  }
+  
+}
